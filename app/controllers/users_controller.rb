4aa21128c2687d7470e77834a6be045921c759fe
yoheihigashi04 <shacy.left04@gmail.com>
2019-12-16 21:55:03 +0800

    ログイン、ログアウト完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user,{only: [:index, :show, :edit, :update]}
-  before_action :forbid_login_user,{only: [:login, :login_form, :new, :create]}
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:login, :login_form, :new, :create]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
 
   def index
     @users = User.all
@@ -65,6 +66,11 @@ class UsersController < ApplicationController
     redirect_to('/login')
   end
 
-
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/new")
+    end
+  end
 
 end
