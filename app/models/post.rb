31df80d34cb2940d0e19fefc1fa4605a90e8b35a
yoheihigashi04 <shacy.left04@gmail.com>
2019-12-17 01:35:38 +0800

    add user_id column on posts db

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :days, {presence: true}
   validates :hours, {presence: true}
   validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_064958) do
+ActiveRecord::Schema.define(version: 2019_12_16_163729) do
 
   create_table "posts", force: :cascade do |t|
     t.date "date"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_14_064958) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
